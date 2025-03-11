c3bc4c679c383f3ed85d945219f0a587749e26d5
karindragimhan49 <147826138+karindragimhan49@users.noreply.github.com>
2025-03-11 13:48:05 +0530

    Resolved merge conflicts

---
+++ Backend/jest.config.js
@@ -0,0 +1,6 @@
+export default {
+  testEnvironment: "node",
+  clearMocks: true,
+  detectOpenHandles: true, // Helps detect unclosed async operations
+  forceExit: true, // Forces Jest to exit after running tests
+};
